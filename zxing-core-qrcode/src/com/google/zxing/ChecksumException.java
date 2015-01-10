65644a6550a573262530d49c08177183315952f2
Charles Zhang <zhangchaoxu@gmail.com>
2015-01-10 19:38:39 +0800

    squash changes

--- zxing-core-qrcode/src/com/google/zxing/ChecksumException.java
+++ zxing-core-qrcode/src/com/google/zxing/ChecksumException.java
@@ -24,14 +24,32 @@ package com.google.zxing;
  */
 public final class ChecksumException extends ReaderException {
 
-  private static final ChecksumException instance = new ChecksumException();
+    private static final ChecksumException instance = new ChecksumException();
 
-  private ChecksumException() {
-    // do nothing
-  }
+    private ChecksumException() {
+        // do nothing
+    }
 
-  public static ChecksumException getChecksumInstance() {
-    return instance;
-  }
+    private ChecksumException(Throwable cause) {
+        super(cause);
+    }
+
+    public static ChecksumException getChecksumInstance() {
+
+        if (isStackTrace) {
+            return new ChecksumException();
+        } else {
+            return instance;
+        }
+    }
+
+    public static ChecksumException getChecksumInstance(Throwable cause) {
+        if (isStackTrace) {
+            return new ChecksumException(cause);
+        } else {
+            return instance;
+        }
+    }
+
 
 }
